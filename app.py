03e243c728e5b1a2c91e5f99088b92fab2529e54
Scheggia <scheggia5071@yahoo.it>
2024-10-24 12:34:34 +0200

    Added print statements and checks for downwind_name

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def select_downwind():
     return render_template('select_downwind.html', downwinds=downwinds)
 
 # Página para registrar un participante
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     try:
         # Obtener el nombre del downwind desde el formulario
